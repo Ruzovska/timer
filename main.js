c7167f72846a5c61740c7391ac31378d4f6b1338
Ruzovska <nastybonda@gmail.com>
2018-09-06 19:23:23 +0700

    Make variables prettier, add default countdownStep.

--- main.js
+++ main.js
@@ -9,30 +9,33 @@ var exec = require('child_process').exec
 //   }
 // });
 
-const second = 1000;
-const minute = 60 * second;
-const hour = 60 * minute;
-const pomodoroSession = 25 * minute;
-const pomodoroBreak = 5 * minute;
+const s = 1000; // second
+const m = 60 * s; // minute
+const h = 60 * m; // hour
+const pS = 25 * m; // pomodoro session
+const pB = 5 * m; // pomodoro break
 var timeUnit = eval(process.argv[2]);
 var timeAmount = eval(process.argv[3]);
 var timeout = timeUnit * timeAmount;
 var countdownStep = eval(process.argv[4]);
+if (countdownStep === undefined) {
+  countdownStep = s; // default countdownStep
+};
 
 //TODO:parallel setIntervals for console.log(countdown) and console.log(Date()) to avoid losing seconds.
 function timer() {
   if (isNaN(countdownStep) === false) {
     var countdown = timeout;
-    var meow = setInterval(function() {
+    var interval = setInterval(function() {
       countdown = countdown - countdownStep;
-      if (countdownStep === minute) {
-        console.log(countdown/minute + ' minutes are left. Time is: ' + Date());
+      if (countdownStep === m) {
+        console.log(countdown/m + ' minutes are left. Time is: ' + Date());
       };
-      if (countdownStep === second) {
-        console.log(countdown/second + ' seconds are left. Time is: ' + Date());
+      if (countdownStep === s) {
+        console.log(countdown/s + ' seconds are left. Time is: ' + Date());
       };
-      if (countdownStep === hour) {
-        console.log(countdown/hour + ' hours are left. Time is: ' + Date());
+      if (countdownStep === h) {
+        console.log(countdown/h + ' hours are left. Time is: ' + Date());
       };
     }, countdownStep);
   };
@@ -41,7 +44,8 @@ function timer() {
     exec('firefox /home/ruzovska/photo/107APPLE/IMG_7430.JPG');
     //exec('firefox \'/home/ruzovska/music/Tomoko\ Aran/亜蘭知子「feel\ so\ free」.mp3\'');
     //exec('firefox \'/home/ruzovska/music/Massive\ Attack/2003\ -\ 100th\ Window/07\ -\ Small\ Time\ Shot\ Away.mp3\'');
-    clearInterval(meow);
+    //exec('firefox \'/home/ruzovska/music/Elsiane/2007\ -\ Hybrid/03.\ Across\ the\ Stream.mp3\'');
+    clearInterval(interval);
   }, timeout);
 };
 timer();
